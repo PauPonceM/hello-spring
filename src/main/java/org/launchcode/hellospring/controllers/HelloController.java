1319cdb8244ea637657f287cab7c24532d9f9af1
Paulina Ponce <paulina.poncemarcano@gmail.com>
2022-08-25 10:41:27 -0400

    first

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,57 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+
+
+//    @GetMapping("hello") //handles request at path /hello
+//    //@GetMapping //this method handles get requests
+//    @ResponseBody //tells spring boot that this method is going to return a plain text response
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye, Spring!";
+    }
+
+    //handles requests of the form /hello?name=LaunchCode (dynamic handler, means accepts data)
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+//Handles request of the form /hello/LaunchCode (the piece of the data is part of the path and not the query
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("form")
+    @ResponseBody
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='hello'>" + //submit a request to /hello
+                "<input type = 'text' name='name'>" +
+                "<select name= 'language'>" +
+                "<option value='French'>French</option>" +
+                "<option value= 'Spanish'> Spanish</option>" +
+                "<option value= 'English'>English</option>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+}
